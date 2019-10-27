6fc586904f76866c27d88735806d4d2bef090f11
Andrew Li <ali@oberlin.edu>
2019-10-27 05:59:30 -0400

    Add documentation

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+/**
+ * Prompts user to set a new definition (val) for an acronym (key)
+ * @param   {string} acronym    the acronym (key) to map the new definition (val) to
+ */
 function setNewDefinition(acronym){
     newDefinition = prompt("Input definition for \"" + acronym + "\":","");
     if (newDefinition != null && newDefinition != "") {
@@ -5,15 +9,20 @@ function setNewDefinition(acronym){
     }
 }
 
+/**
+ * On-click function for translating an acronym
+ * @param   {object} info    Information about the item clicked and the context where the click happened. 
+ *                           Documentation: https://developer.chrome.com/apps/contextMenus
+ */
 function acronymTranslate(info){
     var acronym = info.selectionText.toUpperCase();
     chrome.storage.sync.get({acronym: null}, function(pair){
         var definition = pair.acronym;
-        if (definition != null) {
+        if (definition != null) { // Definition exists
             alert("Translation: \"" + definition + "\".");
-        } else if(acronym.split(" ").length > 1) {
+        } else if(acronym.split(" ").length > 1) { // Multiple words selected
             alert("Multiple words were selected. Please select a single acronym.")
-        } else {
+        } else { // Definition doesn't exist, prompt user on adding to dictionary
             if (confirm("The selected acronym is not in the dictionary. Add to dictionary?")) {
                setNewDefinition(acronym);
             }
@@ -21,17 +30,22 @@ function acronymTranslate(info){
     });
 }
 
+/**
+ * On-click function for mapping an acronym to a definition
+ * @param   {object} info    Information about the item clicked and the context where the click happened. 
+ *                           Documentation: https://developer.chrome.com/apps/contextMenus
+ */
 function addAcronymToDict(info){
     var acronym = info.selectionText.toUpperCase();
     chrome.storage.sync.get({acronym: null}, function(pair){
         var definition = pair.acronym;
-        if (definition != null) { // pair.text accesses the definition
+        if (definition != null) { // Definition exists, prompt user on whether to overwrite
             if (confirm("The selected acronym is already in the dictionary as \"" + definition + "\". Overwrite dictionary definition?")) {
                 setNewDefinition(acronym);
              }
-        } else if(acronym.split(" ").length > 1) {
+        } else if(acronym.split(" ").length > 1) { // Multiple words selected
             alert("Multiple words were selected. Please select a single acronym.")
-        } else {
+        } else { // Definition doesn't exist, add to dictionary
            setNewDefinition(acronym);
         }
     });
@@ -41,18 +55,19 @@ function addAcronymToDict(info){
 function main() {
 
     // Create context menu
-    chrome.contextMenus.create({
+    // Documentation: https://developer.chrome.com/apps/contextMenus
+    chrome.contextMenus.create({ // Parent menu
         title: "Acronym Translate",
         id: "parent",
         contexts:["selection"]
     });
-    chrome.contextMenus.create({
+    chrome.contextMenus.create({ // Menu child
         title: "Translate acronym",
         parentId: "parent",
         contexts:["selection"],
         onclick: acronymTranslate
     });
-    chrome.contextMenus.create({
+    chrome.contextMenus.create({ // Menu child
         title: "Add acronym to dictionary",
         parentId: "parent",
         contexts:["selection"],
